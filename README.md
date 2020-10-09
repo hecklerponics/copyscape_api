487e8b67df46e0f84f1c7ee1860a7979f672210f
Bryan <hecklerponics@gmail.com>
2020-10-09 12:42:34 -0500

    updated documentation and error handling for input of API creds

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 # CopyScape API Connector
 
 A simple connector for checking URLs against [CopyScape's](http://www.copyscape.com/) 
-API for duplication of the content.
+API for duplication of the content. This script will work on Python 3.5+ and returns data in the form of a 
+`pd.DataFrame()`:
+```
+columns =  ['url',  # Matched URL
+            'title',  # Title of the matched URL
+            'min_match_words'  # Minimum number of matched words, 
+            'viewurl',  # CopyScape URL to view comparison
+            'textsnippet'  # Text from positive match
+            'query_url'  # The URL queried for the result
+```
+
+
+## Setting It Up
+The script requires API information to be included in a file named `copyscape_api_key.txt` which contains a
+dictionary with the keys `api_key` and `username`.  To check if your setup is correct use the following to 
+ensure the api creds are being read correctly: 
+
+```python
+import copyscape_connector as cc
+
+report_obj = cc.CopyScapeReport()
+print(report_obj.user_name, report_obj.api_key)
+```
+
+To pull records based on a single URL:
+```python
+url = 'https://www.localseoguide.com'
+
+cc.CopyScapeReport().check_url_for_copies(url)
+```
+
+To pull records for a list of URLs: 
+```python
+url_list = ['https://www.localseoguide.com'
+            'https://www.localu.com/',
+            'https://blumenthals.com/blog/'] 
+
+cc.process_list_of_urls(url_list)
+```
+
+### Current Functionality: 
+- Pull URLs that have been flagged as having duplicated content by providing a list of URLs.
--- copyscape_connector.py
+++ copyscape_connector.py
@@ -1,5 +1,4 @@
 import requests as r
-import urllib
 import xml.etree.ElementTree as ET
 import pandas as pd
 
@@ -8,15 +7,49 @@ class CopyScapeReport:
 
     def __init__(self):
         self.end_point = "https://www.copyscape.com/api/"
-        self.user_name = "localseoguide"
-        self.api_key = "masalnl2v8d1elcr"
+
+        try:
+            self.get_api_info()
+
+        except FileNotFoundError as not_found:
+            print("Missing API Auth File: "
+                  + str(not_found.filename))
+            print("-" * 10)
+            print("""Create a .txt file named 'copyscape_api_key' and 
+                  include a dictionary with the values api_key, and username.""")
+
+            return
 
         self.parameters = {'u': self.user_name,
                            'k': self.api_key}
 
         return
 
+    def get_api_info(self):
+
+        """
+        Reads a text file that contains a dict w/ the copyscape API
+        username and key.
+        """
+
+        import ast
+
+        with open('copyscape_api_key.txt') as f:
+            data = ast.literal_eval(f.read())
+
+        self.api_key = data['api_key']
+        self.user_name = data['username']
+
+        return self
+
     def check_url_for_copies(self, url):
+
+        """
+        Takes a URL and looks for duplicated content
+        based on the provided URL; return a response object
+        and the URL used for the query
+        """
+
         self.parameters['o'] = 'csearch'
         self.parameters['q'] = url
         print(self.parameters)
@@ -25,19 +58,23 @@ class CopyScapeReport:
 
 
 def output_formatted_xml(resp):
+
     """
     Simple function to print out a xml response
     """
+
     root = ET.fromstring(resp.text)
     print(ET.tostring(root, encoding='utf8').decode('utf8'))
     return
 
 
 def parse_xml_report_response(resp):
+
     """
     Parses the copyscape xml for URL results; returns the
     - URL, TITLE, Miniumum Matches Words, VIEWURL (copyscape URi), TEXT SNIPPET
     """
+
     xml_tree = ET.fromstring(resp.text)
     output_dict = {}
 
@@ -54,10 +91,12 @@ def parse_xml_report_response(resp):
 
 
 def process_list_of_urls(url_list):
+
     """
     Simple batch job for taking a list of URL and running them through
-    the job of checking them for duplicates via the copyscape API
+    the job of checking them for duplicates via the copyscape API.
     """
+
     output_df = pd.DataFrame()
 
     for url in url_list:
@@ -68,6 +107,11 @@ def process_list_of_urls(url_list):
 
 
 def create_df_from_results_dict(response, query_url):
+
+    """
+    Takes results response and the query URL and returns a DF.
+    """
+
     result = parse_xml_report_response(response)
     df = pd.DataFrame(result.values(),
                       index=result.keys())
@@ -75,6 +119,3 @@ def create_df_from_results_dict(response, query_url):
     df['query_url'] = query_url
 
     return df
-
-
-
